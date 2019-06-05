16b0dba8ad7c2ead039b0059dfce0e04b43a125a
Süleyman Sevimli <suleyman.sevimli1@gmail.com>
2019-06-06 02:53:09 +0300

    index page design

---
+++ index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html>
+<head>
+	  <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="assets/css/all.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+	<title>VireBase | Hoş Geldiniz.</title>
+</head>
+<body bgcolor="#f8f9fa" >
+	
+
+	<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-white">
+  <a class="navbar-brand pr-3 pl-3" href="#">
+			<img src="https://www.gstatic.com/mobilesdk/160503_mobilesdk/logo/2x/firebase_28dp.png">
+	  	VireBase
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Hoş Geldiniz <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Ürünler</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Fiyatlandırma</a>
+      </li>
+    </ul>
+    <span class="nav-item dropdown pr-5">
+      
+	    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	      <img src="https://cdn0.iconfinder.com/data/icons/kameleon-free-pack-rounded/110/Boss-2-512.png" class="rounded-circle" width="50px" height="50px">
+	    </a>
+	    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+	      <a class="dropdown-item" href="#">Bilgiler</a>
+	      <a class="dropdown-item" href="#">Fiyatlandırma</a>
+	      <a class="dropdown-item" href="#">Çıkış Yap</a>
+	    </div>
+      
+    </span>
+  </div>
+</nav>
+
+	<!-- #Navbar End -->
+	
+	<!-- Release -->
+	<div class="col-md-12 pt-5 pl-5 pb-3">
+		<div class="row">
+			<div class="col-md-7 pt-4 pl-3">
+				<h4 class="h4">
+					VireBase'e hoş geldiniz.
+				</h4>
+				<p>Harika uygulamalar geliştirmek, kullanıcılarınızla etkileşim kurmak ve mobil reklamlarla daha fazla kazanmak için VireBase'den araçlar.</p>
+				<a href="#" class="link">Daha Fazla Bilgi Edinin</a>
+				<a href="#" class="link ml-5">Belgeler</a>
+				<a href="#" class="link ml-5">Destek</a>
+			</div>
+			<div class="col-md-5">
+				<img class="img-responsive" src="images/database.png">
+			</div>
+		</div>
+	</div>
+	<!-- #Release -->
+
+	<!-- New Product -->
+	<div class="col-md-12 mx-auto pt-3 bg-light">
+		<h4 class="h4 mt-5 text-center">Yeni Bir Proje Oluşturun</h4>
+		<h6 class="h6 text-muted text-center mb-5"> İster Yeni Bir proje oluşturun isterseniz de eski projelerinizi geliştirin. </h6>
+		<div class="col-md-8 mx-auto mt-5">
+			<div class="row mx-auto">
+				<div class="col-md-4 mx-auto">
+					<div class="card mb-3" style="max-width: 18rem;">
+					  <div class="card-body ">
+					    <h5 class="card-title">Yeni Proje Oluşturun</h5>
+					    <p class="card-text text-center mx-auto align-middle">
+					    	<i style="font-size:24pt; color:grey;" class=" mt-1 fas fa-plus-circle"></i>
+					    </p>
+					  </div>
+					  	<div class="card-footer mt-5 text-left bg-transparent border-default btn btn-default">
+					  		<i class="fas fa-compass"></i>
+					  		Demo Bir Projeyi Keşfet
+						</div>
+					</div>
+
+				</div>
+				<div class="col-md-4">
+					<div class="card mb-3" style="max-width: 18rem;">
+					  <div class="card-body ">
+					    <h5 class="card-title">Project 1</h5>
+					    <p class="card-text">suleymansevimli</p>
+					  </div>
+					  <div class="card-footer mt-5 text-left bg-transparent border-default btn btn-default">  
+					  	<i class="fab fa-android"></i> 
+					  </div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card mb-3" style="max-width: 18rem;">
+					  <div class="card-body ">
+					    <h5 class="card-title">Project 2</h5>
+					    <p class="card-text ">joedoe</p>
+					  </div>
+					  <div class="card-footer mt-5 text-left bg-transparent border-default btn btn-default"> 
+					  	<i class="fab fa-apple"></i> &nbsp;  
+					  	<i class="fab fa-chrome"></i> 
+					  </div>
+					</div>
+				</div>
+				<div class="col-md-4 mx-auto">
+					<div class="card mb-3" style="max-width: 18rem;">
+					  <div class="card-body ">
+					    <h5 class="card-title">Project 3</h5>
+					    <p class="card-text">suleymansevimli</p>
+					  </div>
+					  <div class="card-footer mt-5 text-left bg-transparent border-default btn btn-default">
+					  	<i class="fab fa-apple"></i> &nbsp;
+					  	<i class="fab fa-android"></i>
+					  </div>
+					</div>
+
+				</div>
+				<div class="col-md-4">
+					<div class="card mb-3" style="max-width: 18rem;">
+					  <div class="card-body ">
+					    <h5 class="card-title">Project 4</h5>
+					    <p class="card-text">joedoe</p>
+					  </div>
+					  <div class="card-footer mt-5 text-left bg-transparent border-default btn btn-default">  
+					  	<i class="fab fa-chrome"></i> &nbsp;
+					  	<i class="fab fa-apple"></i> &nbsp;
+					  	<i class="fab fa-android"></i>
+					  </div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card mb-3" style="max-width: 18rem;">
+					  <div class="card-body ">
+					    <h5 class="card-title">Project 5</h5>
+					    <p class="card-text">suleymansevimli</p>
+					  </div>
+					  <div class="card-footer mt-5 text-left bg-transparent border-default btn btn-default"> 
+					  	<i class="fab fa-apple"></i> 
+					  </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- #New Product -->
+	
+	<!-- Footer -->
+	<footer class="mt-3 mb-5">
+		<div class="col-md-8 mx-auto text-center mt-5">
+			<a href="#" class="link text-muted"> Şartlar </a> &nbsp; &nbsp;
+			<a href="#" class="link text-muted"> Gizlilik Politikası </a>
+		</div>
+	</footer>
+	<!-- #Footer -->
+
+
+<!-- Script -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+</body>
+</html>
